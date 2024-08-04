7fe54f431169f2d51357a47c579aeb13f9e8b969
Tomas Ribes <tomasribesb@gmail.com>
2024-08-04 17:38:17 -0300

    bases

---
+++ src/presentation/navigator/StackNavigator.tsx
@@ -0,0 +1,24 @@
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from '../screens/home/HomeScreen';
+import { SearchScreen } from '../screens/search/SearchScreen';
+import { PokemonScreen } from '../screens/pokemon/PokemonScreen';
+
+
+export type RootStackParam = {
+    HomeScreen: undefined;
+    PokemonScreen: { pokemonId: number };
+    SearchScreen: undefined;
+}
+
+
+const Stack = createStackNavigator();
+
+export const StackNavigator = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="PokemonScreen" component={PokemonScreen} />
+            <Stack.Screen name="SearchScreen" component={SearchScreen} />
+        </Stack.Navigator>
+    );
+}
