92098bbdcf9b94aede783d72742f1f5f5e01e751
Tomas Ribes <tomasribesb@gmail.com>
2024-08-04 18:38:48 -0300

    axios + cache con staclquery

--- src/PokedexApp.tsx
+++ src/PokedexApp.tsx
@@ -1,11 +1,16 @@
 import 'react-native-gesture-handler';
 import { StackNavigator } from './presentation/navigator/StackNavigator';
 import { ThemecontextProvider } from './presentation/context/ThemeContext';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient()
 
 export const PokedexApp = () => {
     return (
-        <ThemecontextProvider>
+        <QueryClientProvider client={queryClient}>
+            <ThemecontextProvider>
                 <StackNavigator />
-        </ThemecontextProvider>
+            </ThemecontextProvider>
+        </QueryClientProvider>
     )
 }
